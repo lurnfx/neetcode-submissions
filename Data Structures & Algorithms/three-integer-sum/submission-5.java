ea59c6fa93ec5ffb3702e118ee4381ed6e6446b9
Bhushan <80579471+lurnfx@users.noreply.github.com>
2026-04-21 21:56:15 +0530

    Add: three-integer-sum - submission-5

---
+++ Data Structures & Algorithms/three-integer-sum/submission-5.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        Arrays.sort(nums);   //1,2,2,3,2,5;
+       
+        for (int i = 0; i < nums.length; i++) {
+
+            //skip the duplicate number
+            if(i > 0 && nums[i]==nums[i-1]) continue;
+
+            int left = i+1;
+            int right = nums.length-1;
+
+            while(left<right){
+               int sum = nums[i]+nums[left]+nums[right];
+               if(sum > 0){
+                 right--;
+               }else if(sum < 0){
+                left++;
+               }else {
+                  res.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                  //needs to move the pointer to the right 
+                  left++;
+                  //dduplicate check
+                  while(nums[left]==nums[left-1] && left<right){
+                    left++;
+                  }
+               }
+            }  
+        }
+ 
+         return res;
+    }
+
+    //optimise solution in O(n2) and time complexity is O(log n) or O(1);
+}
